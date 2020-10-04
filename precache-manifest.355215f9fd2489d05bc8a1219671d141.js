bbed8e32c4d4fdb8bb8fc7287de38a7f96249848
Maxim <turbo3.6@cn.ru>
2020-10-04 16:38:31 +0700

    Updates

---
+++ precache-manifest.355215f9fd2489d05bc8a1219671d141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbf2fc64321d619674a2820b2ed0e5c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1893e64a10d6df62788d",
+    "url": "/portfolio/static/css/main.ee6c472e.chunk.css"
+  },
+  {
+    "revision": "a51a8827f8abd64aa0aa",
+    "url": "/portfolio/static/js/2.30126f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.30126f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1893e64a10d6df62788d",
+    "url": "/portfolio/static/js/main.3bb139ec.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
